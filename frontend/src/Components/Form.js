68ce1658d80e55779cc79ba3228611b18ab36365
Jenwng <Jenniescwong@gmail.com>
2020-03-08 17:35:33 -0400

    setting up ethnicity radio format

--- frontend/src/Components/Form.js
+++ frontend/src/Components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Datepicker from "react-bootstrap-date-picker";
+import { Redirect } from "react-router-dom";
 
 class Form extends Component {
   constructor(props) {
@@ -12,7 +13,12 @@ class Form extends Component {
       city: "",
       state: "",
       zip: "",
-      birthday: "",
+      birthdate: function() {
+        let birthDate = new Date().toISOString();
+        return {
+          birthdate: birthDate
+        };
+      },
       ethnicity: "",
       linkedin: "",
       github: "",
@@ -28,19 +34,59 @@ class Form extends Component {
       laptop: true,
       whyinterested: "",
       howheard: "",
-      skilllevel: ""
+      skilllevel: "",
+      redirect: false
     };
 
-    // const App = React.createClass({
-    //     getInitialState: function(){
-    //      var value = new Date().toISOString();
-    // }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event, formattedValue) {
+    event.preventDefault();
+    this.setState({ name: event.target.value });
+    this.setState({ phone: event.target.value });
+    this.setState({ address: event.target.value });
+    this.setState({ city: event.target.value });
+    this.setState({ state: event.target.value });
+    this.setState({ zip: event.target.value });
+    this.setState({
+      birthdate: event.target.value,
+      formattedValue: formattedValue
+    });
+    this.setState({ ethnicity: event.target.value });
+    this.setState({ linkedin: event.target.value });
+    this.setState({ github: event.target.value });
+    this.setState({ extralink: event.target.value });
+    this.setState({ coverletter: event.target.value });
+    this.setState({ education: event.target.value });
+    this.setState({ collegemajor: event.target.value });
+    this.setState({ collegelocation: event.target.value });
+    this.setState({ isemployed: event.target.value });
+    this.setState({ employer: event.target.value });
+    this.setState({ military: event.target.value });
+    this.setState({ income: event.target.value });
+    this.setState({ laptop: event.target.value });
+    this.setState({ whyinterested: event.target.value });
+    this.setState({ howheard: event.target.value });
+    this.setState({ skilllevel: event.target.value });
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+    this.setState({ redirect: true });
+  }
+
+  componentDidUpdate() {
+    let hiddenInputElement = document.getElementById("user-birthdate");
+    console.log(hiddenInputElement.value);
+    console.log(hiddenInputElement.getAttribute("data-formattedvalue"));
   }
 
   render() {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formBasicName">
             <Form.Label>Name:</Form.Label>
             <Form.Control type="email" placeholder="Enter full name" />
@@ -74,7 +120,47 @@ class Form extends Component {
               <Form.Control />
             </Form.Group>
           </Form.Row>
-          <Form.Group controlId="formBirthday"></Form.Group>
+          <Form.Group controlId="formBirthday">
+            <ControlLabel>Birthdate: </ControlLabel>
+            <Datepicker
+              id="user-birthdate"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <fieldset>
+            <Form.Group as={Row} controlId="formEthinicity">
+              <Form.Label as="legend" column sm={2}>
+                Ethnicity:
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="Hispanics"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label="African American"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Asian"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios3"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Other"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios4"
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
         </Form>
       </div>
     );
