6714a126c5cc7cb6d300164e98a1a5071a6c18ee
Jenwng <Jenniescwong@gmail.com>
2020-03-11 13:58:27 -0400

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import "./App.css";
-import Forms from './Components/Forms';
-import Submitted from './Components/Submitted';
-import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from "./Components/NavBar";
+import Forms from "./Components/Forms";
+import Submitted from "./Components/Submitted";
+import { BrowserRouter, Route } from "react-router-dom";
 // import "bootstrap/dist/css/boostrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <NavBar />
         <div className="container">
           <div className="page-header">
-            <h1>Testing</h1>
+            <h1>Applicant Management System</h1>
           </div>
           <Route path="/form-create" component={Forms} />
           <Route path="/form-submitted" component={Submitted} />
