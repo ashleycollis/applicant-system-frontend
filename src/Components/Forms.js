3b1648659290824866820d106d85f8b41f263888
Jenwng <Jenniescwong@gmail.com>
2020-03-16 13:49:01 -0400

    fixed the axios post request, now working on submitted component

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -22,7 +22,7 @@ class Forms extends Component {
   constructor() {
     super();
     this.state = {
-      cohort_id: "",
+      // cohort_id: "",
       name: "",
       email: "",
       phone: "",
@@ -40,17 +40,17 @@ class Forms extends Component {
       highest_degree: "",
       college_major: "",
       college_location: "",
-      is_employed: "",
+      is_employed: false,
       employer: "",
-      is_military: "",
+      is_military: false,
       income: "",
-      has_laptop: "",
+      has_laptop: false,
       why_interested: "",
       how_heard: "",
       skill_level: "",
       app_status: "",
-      reviewer_comments: "",
-      redirect: false
+      reviewer_comments: ""
+      // redirect: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -64,7 +64,38 @@ class Forms extends Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-    const applicantData = this.setState({ redirect: true });
+    // const applicantData = {
+    //   name: this.state.name,
+    //   email: this.state.email,
+    //   phone: this.state.phone,
+    //   address: this.state.address,
+    //   city: this.state.city,
+    //   state: this.state.state,
+    //   zip: this.state.zip,
+    //   birth_date: this.state.birth_date,
+    //   gender: this.state.gender,
+    //   ethnicity: this.state.ethnicity,
+    //   linkedin: this.state.linkedin,
+    //   github: this.state.github,
+    //   extra_link: this.state.extra_link,
+    //   cover_letter: this.state.cover_letter,
+    //   highest_degree: this.state.highest_degree,
+    //   college_major: this.state.college_major,
+    //   college_location: this.state.college_location,
+    //   is_employed: this.state.is_employed,
+    //   employer: this.state.employer,
+    //   is_military: this.state.is_military,
+    //   income: this.state.income,
+    //   has_laptop: this.state.has_laptop,
+    //   why_interested: this.state.why_interested,
+    //   how_heard: this.state.how_heard,
+    //   skill_level: this.state.skill_level,
+    //   app_status: this.state.app_status,
+    //   reviewer_comments: this.state.reviewer_comments,
+    //   redirect: true
+    // };
+    const applicantData = this.state;
+    console.log(applicantData);
     const newData = await axios.post(
       "http://localhost:3000/applications",
       applicantData
@@ -79,9 +110,9 @@ class Forms extends Component {
   };
 
   handleDate = date => {
-    let dbFriendlyDate = date.toISOString();
+    // let dbFriendlyDate = date.toISOString();
     this.setState({
-      birthdate: dbFriendlyDate
+      birth_date: date
     });
   };
 
